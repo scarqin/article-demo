777c13198bacb621c80ba9d86db2ae4b2cfd19d1
scar <1054139596@qq.com>
2020-08-12 23:33:51 +0800

    feat footer add grip layout

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Usage
+----
+文章里面的例子
+
+List
+---
+* [CSS Footer固定到底部](https://juejin.im/post/6844903559922712583)- [例子](http://htmlpreview.github.io/?https://github.com/scarqin/footer-demo/blob/master/footer/method.html)
+
+* [锚点定位被顶部固定导航栏遮住的解决方案](https://juejin.im/post/6844904118738223117)- [例子](http://htmlpreview.github.io/?https://github.com/scarqin/footer-demo/blob/master/fixedMenuAndAnchor)
--- footer/js/test.js
+++ footer/js/test.js
@@ -1,25 +1,48 @@
-$(function() {
+$(function () {
 	var footer = $('.footer')
-	$('#btn-absolute').click(function() {
-		$('.wrapper').removeClass('wrapper-add');
-		$('.push').removeClass('push-add');
+
+	function resetClass() {
+		$('.wrapper').removeClass('wrapper-push');
+		$('.push').removeClass('push-push');
+		$('body').removeClass('body-flex');
+		$('body').removeClass('body-grid');
+		$('.wrapper').removeClass('wrapper-flex');
 		footer.removeClass('relative')
-		footer.addClass('absolute');
-	})
-	$('#btn-relative').click(function() {
-		$('.wrapper').removeClass('wrapper-add');
-		$('.push').removeClass('push-add');
 		footer.removeClass('absolute')
-		footer.addClass('relative');
-	})
-	$('#btn-add').click(function() {
+	}
+
+	$('#btn-add').click(function () {
 		var box = $('#main'),
 			str = '';
 		str = '<table class="table"><caption>Optionaltablecaption.</caption><thead><tr><th>#</th><th>FirstName</th><th>LastName</th><th>Username</th></tr></thead><tbody><tr><th scope="row">1</th><td>Mark</td><td>Otto</td><td>@mdo</td></tr><tr><th scope="row">2</th><td>Jacob</td><td>Thornton</td><td>@fat</td></tr><tr><th scope="row">3</th><td>Larry</td><td>theBird</td><td>@twitter</td></tr></tbody></table>'
 		box.append(str);
 	})
-	$('#btn-push').click(function() {
-		$('.wrapper').addClass('wrapper-add');
-		$('.push').addClass('push-add');
+	$('#btn-delete').click(function () {
+		var box = $('.table');
+		if (box.length) {
+			box[0].remove()
+		}
+	})
+	$('#btn-absolute').click(function () {
+		resetClass();
+		footer.addClass('absolute');
+	})
+	$('#btn-relative').click(function () {
+		resetClass()
+		footer.addClass('relative');
+	})
+	$('#btn-push').click(function () {
+		resetClass();
+		$('.wrapper').addClass('wrapper-push');
+		$('.placeholder-div').addClass('placeholder-push');
+	})
+	$('#btn-flex').click(function () {
+		resetClass();
+		$('body').addClass('body-flex');
+		$('.wrapper').addClass('wrapper-flex');
+	})
+	$('#btn-grid').click(function () {
+		resetClass();
+		$('body').addClass('body-grid');
 	})
 })
